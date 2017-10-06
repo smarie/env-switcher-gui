bcae8d61bc60693540303208496ef9c0c78b9871
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-10-06 13:42:32 +0200

    Finally making travis compile qt and pyqt + cache the compilation results ?

--- ci_tools/build_install_qt.sh
+++ ci_tools/build_install_qt.sh
@@ -2,20 +2,24 @@
 
 # before running this script the variable QT_DIR should exist.
 echo "***** Starting compilation of Qt for installation to $QT_DIR *******"
-export QT_SRC_DIR="$TRAVIS_BUILD_DIR/../qt-src"
-export QT_SRC_URL="http://download.qt.io/official_releases/qt/5.6/5.6.3/single/qt-everywhere-opensource-src-5.6.3.tar.xz"
+export QT_MAJ=5
+export QT_MIN=6
+export QT_PAT=3
+export QT_ARCHIVE="qt-everywhere-opensource-src-$QT_MAJ.$QT_MIN.$QT_PAT"
+export QT_SRC_URL="http://download.qt.io/official_releases/qt/$QT_MAJ.$QT_MIN/$QT_MAJ.$QT_MIN.$QT_PAT/single/$QT_ARCHIVE.tar.xz"
+export QT_SRC_DIR="$TRAVIS_BUILD_DIR/../$QT_ARCHIVE"
 
 echo "(a) Downloading Qt sources from $QT_SRC_URL in "
 pwd
-wget -q $QT_SRC_URL -O qt-src.tar.xz
+wget -q $QT_SRC_URL
 
 echo "(b) Unzipping Qt sources in "
 pwd
-tar -xvf qt-src.tar.xz >/dev/null
+tar -xvf $QT_ARCHIVE.tar.xz >/dev/null
 
 echo "(c) Installing Qt dependencies "
 sudo apt-get install libxcb*
-# from https://wiki.qt.io/Install_Qt_5_on_Ubuntu: install opengl libraries so as to be able to build QtGui
+# from https://wiki.qt.io/Install_Qt_$QT_MAJ_on_Ubuntu: install opengl libraries so as to be able to build QtGui
 sudo apt-get install mesa-common-dev
 sudo apt-get install libglu1-mesa-dev -y
 
@@ -26,7 +30,7 @@ sudo ./configure -opensource -confirm-license -prefix $QT_DIR -no-icu -no-cups -
 
 ## Note: to get the list of modules (to use with -skip): find . -regextype sed -maxdepth 1 -regex "./qt[^b^\.].*"
 ## Note: -no-dbus does not work, it still looks for it later in the compilation
-## Fine-tuning: features from this list could be disabled one by one : https://github.com/qt/qtbase/blob/v5.6.3/src/corelib/global/qfeatures.txt
+## Fine-tuning: features from this list could be disabled one by one : https://github.com/qt/qtbase/blob/v$QT_MAJ.$QT_MIN.$QT_PAT/src/corelib/global/qfeatures.txt
 #-no-feature-TEXTHTMLPARSER -no-feature-TEXTODFWRITER -no-feature-CSSPARSER -no-feature-XMLSTREAM -no-feature-XMLSTREAMREADER -no-feature-XMLSTREAMWRITER -no-feature-DOM -no-feature-TREEWIDGET -no-feature-TABLEWIDGET -no-feature-TEXTBROWSER -no-feature-FONTCOMBOBOX -no-feature-TOOLBAR -no-feature-TOOLBOX -no-feature-GROUPBOX -no-feature-DOCKWIDGET -no-feature-MDIAREA -no-feature-SLIDER -no-feature-SCROLLBAR -no-feature-DIAL -no-feature-SCROLLAREA -no-feature-GRAPHICSVIEW -no-feature-GRAPHICSEFFECT -no-feature-SPINWIDGET -no-feature-TEXTEDIT -no-feature-SYNTAXHIGHLIGHTER -no-feature-RUBBERBAND -no-feature-CALENDARWIDGET -no-feature-PRINTPREVIEWWIDGET -no-feature-FONTDIALOG -no-feature-PRINTDIALOG -no-feature-PRINTPREVIEWDIALOG -no-feature-TABLEVIEW -no-feature-TREEVIEW -no-feature-IMAGEFORMATPLUGIN -no-feature-MOVIE -no-feature-IMAGEFORMAT_BMP -no-feature-IMAGEFORMAT_PPM -no-feature-IMAGEFORMAT_XBM -no-feature-IMAGEFORMAT_XPM -no-feature-IMAGE_HEURISTIC_MASK -no-feature-PICTURE -no-feature-COLORNAMES -no-feature-PRINTER -no-feature-CUPS -no-feature-PAINT_DEBUG -no-feature-FTP -no-feature-HTTP -no-feature-UDPSOCKET -no-feature-NETWORKINTERFACE -no-feature-NETWORKDISKCACHE -no-feature-COMPLETER -no-feature-FSCOMPLETER -no-feature-DESKTOPSERVICES -no-feature-ANIMATION -no-feature-STATEMACHINE
 
 echo "(e) Building pcre "
